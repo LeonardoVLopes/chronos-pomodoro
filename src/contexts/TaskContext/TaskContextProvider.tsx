91ae2e996a85f65aea10c1d7d220e74833f3ff1c
leo <leonardolopes565@gmail.com>
2026-01-18 20:02:55 -0300

    Tocando áudio ao completar uma tarefa (JavaScript Audio)

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,10 +1,11 @@
 import type React from "react";
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useRef } from "react";
 
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 import { TaskReducer } from "./taskReducer";
 import { TimerWorkerManager } from "../../workers/TimerWorkerManager";
+import { loadBeep } from "../../utils/loadBeep";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -12,15 +13,19 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispath] = useReducer(TaskReducer, initialTaskState);
+  let playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
   const worker = TimerWorkerManager.getInstance();
 
   useEffect(() => {
     worker.onmessage((e) => {
       const countDownSeconds = e.data;
-      console.log(countDownSeconds);
 
       if (countDownSeconds <= 0) {
+        if (playBeepRef.current) {
+          playBeepRef.current();
+          playBeepRef.current = null;
+        }
         dispath({ type: "COMPLETE_TASK" });
         worker.terminate();
       } else {
@@ -29,19 +34,24 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
           payload: { secondsRemaining: countDownSeconds },
         });
       }
-
-      console.log(e.data);
     });
   }, [worker, dispath]);
 
   useEffect(() => {
     if (!state.activeTask) {
-      console.log("worker terminado por falta de activeTask");
       worker.terminate();
     }
 
     worker.postMessage(state);
   }, [worker, state]);
+
+  useEffect(() => {
+    if (state.activeTask && playBeepRef.current === null) {
+      playBeepRef.current = loadBeep();
+    } else {
+      playBeepRef.current = null;
+    }
+  }, [state.activeTask]);
 
   return (
     <TaskContext.Provider value={{ state, dispath }}>
