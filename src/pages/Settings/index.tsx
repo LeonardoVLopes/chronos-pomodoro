33c284cd68fad9b3bfcc4731a34638244c556bb0
leo <leonardolopes565@gmail.com>
2026-01-30 22:21:41 -0300

    Validando os campos do formulário antes de salvar os dados

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -4,13 +4,56 @@ import { DefaultButton } from "../../components/DefaultButton";
 import { DefaultInput } from "../../components/DefaultInput";
 import { Heading } from "../../components/Heading";
 import { MainTemplate } from "../../templates/MainTemplate";
+import { useRef } from "react";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { showMessage } from "../../adapters/showMessage";
 
 export function Settings() {
+  const { state } = useTaskContext();
+  const workTimeInput = useRef<HTMLInputElement>(null);
+  const shortBreakTimeInput = useRef<HTMLInputElement>(null);
+  const longBreakTimeInput = useRef<HTMLInputElement>(null);
+
+  function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    showMessage.dissmiss();
+
+    const formErrors = [];
+
+    const workTime = Number(workTimeInput.current?.value);
+    const shortBreakTime = Number(shortBreakTimeInput.current?.value);
+    const longBreakTime = Number(longBreakTimeInput.current?.value);
+
+    if (isNaN(workTime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
+      formErrors.push("por favor insira apenas numeros para todos os campos");
+    }
+
+    if (workTime < 1 || workTime > 99) {
+      formErrors.push("digite valores entre 1 e 99 para foco");
+    }
+
+    if (shortBreakTime < 1 || shortBreakTime > 30) {
+      formErrors.push("digite valores entre 1 e 30 para descanso curto");
+    }
+
+    if (longBreakTime < 1 || longBreakTime > 60) {
+      formErrors.push("digite valores entre 1 e 60 para descanso longo");
+    }
+
+    if (formErrors.length > 0) {
+      formErrors.forEach((error) => {
+        showMessage.error(error);
+      });
+      return;
+    }
+  }
+
   return (
     <MainTemplate>
       <Container>
         <Heading>Configurações</Heading>
       </Container>
+
       <Container>
         <p style={{ textAlign: "center" }}>
           Modifique as configurações para o tempo de foco, descanso curso e
@@ -19,18 +62,40 @@ export function Settings() {
       </Container>
 
       <Container>
-        <form action="" className="form">
+        <form onSubmit={handleSaveSettings} action="" className="form">
           <div className="formRow">
-            <DefaultInput id="workTime" labelText="Foco" />
+            <DefaultInput
+              id="workTime"
+              labelText="Foco"
+              ref={workTimeInput}
+              defaultValue={state.config.workTime}
+              type="number"
+            />
           </div>
           <div className="formRow">
-            <DefaultInput id="shortBreakTime" labelText="Descanso Curto" />
+            <DefaultInput
+              id="shortBreakTime"
+              labelText="Descanso Curto"
+              ref={shortBreakTimeInput}
+              defaultValue={state.config.shortBreakTime}
+              type="number"
+            />
           </div>
           <div className="formRow">
-            <DefaultInput id="longBreakTime" labelText="Descanso Longo" />
+            <DefaultInput
+              id="longBreakTime"
+              labelText="Descanso Longo"
+              ref={longBreakTimeInput}
+              defaultValue={state.config.longBreakTime}
+              type="number"
+            />
           </div>
           <div className="formRow">
-            <DefaultButton icon={<SaveIcon />} aria-label="Salvar configuracoes" title="Salvar configuracoes"/>
+            <DefaultButton
+              icon={<SaveIcon />}
+              aria-label="Salvar configuracoes"
+              title="Salvar configuracoes"
+            />
           </div>
         </form>
       </Container>
